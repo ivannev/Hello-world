ef11ad93c2b5574d67c9afcc3795a6d569404df5
ivannev <ivan.nevostruev96@gmail.com>
2020-02-21 15:59:42 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int a = 1;
+       int b = 2;
+       int c = a + b;
+        System.out.println("c=" + c);
+        System.out.println("a > b=" + (a > b));
+        System.out.println("a < b=" + (a < b));
+        String hello = "hello";
+        String world = "world";
+        System.out.println(hello + " "+world+"!");
+    }
+}
